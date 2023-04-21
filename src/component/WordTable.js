39ba71b3b482b02dcfd07c729ab0ce4715750754
Taeyeong Sun <styd4957@gmail.com>
2023-04-22 07:03:59 +0900

    20230422

--- src/component/WordTable.js
+++ src/component/WordTable.js
@@ -6,20 +6,40 @@ function WordTable() {
     selectRow();
   }, []);
 
+  const phpHost = 'http://localhost';
+  const phpUrl = {
+    autoIncre: phpHost + '/php/word/autoIncre.php',
+    select: phpHost + '/php/word/select.php',
+    insert: phpHost + '/php/word/insert.php',
+    update: phpHost + '/php/word/update.php',
+    delete: phpHost + '/php/word/delete.php'
+  };
   const column = { no: 0, language: '', level: '', chapter: '', gubun: '', kl: '', cl: '', el: '', rl: '', date: '' };
-  const [rowMain, setRowMain] = useState(column);
+  const [rowm, setRowm] = useState(column);
   const [rows, setRows] = useState([]);
 
-  function toEng(value) {
-    return value.replace(/[^a-zA-Z]/g, '');
+  function handleInputRowm(event) {
+    let value = event.target.value;
+    if (event.target.pattern == '^[0-9]*$')
+      value = value.replace(/[^0-9]/g, "");
+    else if (event.target.pattern == '^[a-zA-Z]*$')
+      value = value.replace(/[^A-Za-z]/g, "");
+    setRowm((prevState) => ({ ...prevState, [event.target.name]: value }));
   }
 
-  function toNum(value) {
-    return value.replace(/[^0-9]/g, '');
+  function handleInputRows(event, index) {
+    let value = event.target.value;
+    if (event.target.pattern == '^[0-9]*$')
+      value = value.replace(/[^0-9]/g, "");
+    else if (event.target.pattern == '^[a-zA-Z]*$')
+      value = value.replace(/[^A-Za-z]/g, "");
+    setRows((prevState) => [...prevState].map((v, i) => i === index ? ({ ...prevState[index], [event.target.name]: value, btnOpt: true }) : v));
   }
 
-  function newArr(index, value) {
-    return [...rows].map((v, i) => i === index ? value : v);
+  async function initializeRowm() {
+    const result = await post(phpUrl.autoIncre, null);
+    if (result != '')
+      setRowm({ ...column, no: result, date: getDate() });
   }
 
   function getDate() {
@@ -30,150 +50,139 @@ function WordTable() {
     return `${year}-${month}-${date}`;
   }
 
-  function message(index) {
-    if (index == null) {
-      return (
-        `번호 : ${rowMain.no}\n` +
-        `언어 : ${rowMain.language}\n` +
-        `레벨 : ${rowMain.level}\n` +
-        `단원 : ${rowMain.chapter}\n` +
-        `구분 : ${rowMain.gubun}\n` +
-        `한국어 : ${rowMain.kl}\n` +
-        `중국어 : ${rowMain.cl}\n` +
-        `영어 : ${rowMain.el}\n` +
-        `러시아어 : ${rowMain.rl}\n` +
-        `갱신일자 : ${rowMain.date}`);
+  async function selectRow() {
+    const result = await post(phpUrl.select, null);
+    if (result != '') {
+      setRows(result.map((row) => ({ ...row, btnOpt: false })));
+      initializeRowm();
     }
+  }
+
+  async function insertRow() {
+    if (rowm.language.length < 2 || rowm.level.length < 1 || rowm.chapter.length < 2 || rowm.gubun.length < 1)
+      alert('언어, 레벨, 단원, 구분 값은 필수 입력사항입니다');
     else {
-      return (
-        `번호 : ${rows[index].no}\n` +
-        `언어 : ${rows[index].language}\n` +
-        `레벨 : ${rows[index].level}\n` +
-        `단원 : ${rows[index].chapter}\n` +
-        `구분 : ${rows[index].gubun}\n` +
-        `한국어 : ${rows[index].kl}\n` +
-        `중국어 : ${rows[index].cl}\n` +
-        `영어 : ${rows[index].el}\n` +
-        `러시아어 : ${rows[index].rl}\n` +
-        `갱신일자 : ${rows[index].date}`);
-    }
-  }
-
-  function initializeRowMain() {
-    fetch('http://localhost/php/word/autoIncre.php', { method: 'POST' })
-      .then((respons) => respons.json())
-      .then((result) => setRowMain({ ...column, no: result, date: getDate() }));
-  }
-
-  function selectRow() {
-    fetch('http://localhost/php/word/select.php', { method: 'POST' })
-      .then((respons) => respons.json())
-      .then((result) => setRows(result.map((row) => ({ ...row, btnOpt: false }))))
-      .then(() => initializeRowMain());
-  }
-
-  function insertRow() {
-    let alertMsg = '';
-    if (rowMain.language.length < 2) alertMsg += `'언어' 값은 2자 이상이어야 합니다.\n\n`;
-    if (rowMain.level.length < 1) alertMsg += `'레벨' 값은 1자 이상이어야 합니다.\n\n`;
-    if (rowMain.chapter.length < 2) alertMsg += `'단원' 값은 2자 이상이어야 합니다.\n\n`;
-    if (rowMain.gubun.length < 1) alertMsg += `'구분' 값은 1자 이상이어야 합니다.\n\n`;
-    if (alertMsg !== '') {
-      alertMsg = alertMsg.slice(0, -2);
-      alert(alertMsg);
-    }
-    else {
-      if (window.confirm(message(null))) {
-        const row = { ...rowMain };
-        const data = new URLSearchParams(row);
-        fetch('http://localhost/php/word/insert.php', { method: 'POST', body: data })
-          .then(() => setRows([...rows, { ...row, btnOpt: false }]))
-          .then(() => initializeRowMain());
+      const value = { ...rowm };
+      const result = await post(phpUrl.insert, value);
+      if (result != '') {
+        setRows((prevState) => [...prevState, { ...value, btnOpt: false }]);
+        initializeRowm();
       }
     }
   }
 
-  function updateRow(index) {
-    let alertMsg = '';
-    if (rows[index].language.length < 2) alertMsg += `'언어' 값은 2자 이상이어야 합니다.\n\n`;
-    if (rows[index].level.length < 1) alertMsg += `'레벨' 값은 1자 이상이어야 합니다.\n\n`;
-    if (rows[index].chapter.length < 2) alertMsg += `'단원' 값은 2자 이상이어야 합니다.\n\n`;
-    if (rows[index].gubun.length < 1) alertMsg += `'구분' 값은 1자 이상이어야 합니다.\n\n`;
-    if (alertMsg !== '') {
-      alertMsg = alertMsg.slice(0, -2);
-      alert(alertMsg);
-    }
+  async function updateRow(index) {
+    if (rows[index].language.length < 2 || rows[index].level.length < 1 || rows[index].chapter.length < 2 || rows[index].gubun.length < 1)
+      alert('언어, 레벨, 단원, 구분 값은 필수 입력사항입니다');
     else {
-      if (window.confirm(message(index))) {
-        const row = { ...rows[index] };
-        const data = new URLSearchParams(row);
-        fetch('http://localhost/php/word/update.php', { method: 'POST', body: data })
-          .then(() => setRows(newArr(index, { ...row, btnOpt: false })));
-      }
+      const value = { ...rows[index] };
+      const result = await post(phpUrl.update, value);
+      if (result != '')
+        setRows((prevState) => [...prevState].map((v, i) => i === index ? ({ ...value, btnOpt: false }) : v));
     }
   }
 
-  function deleteRow(index) {
-    if (window.confirm(message(index))) {
-      const row = { ...rows[index] };
-      const data = new URLSearchParams(row);
-      fetch('http://localhost/php/word/delete.php', { method: 'POST', body: data })
-        .then(() => setRows(rows.filter((_, i) => i !== index)));
-    }
+  async function deleteRow(index) {
+    const value = { ...rows[index] };
+    const result = await post(phpUrl.delete, value);
+    if (result !== '')
+      setRows((prevState) => prevState.filter((_, i) => i !== index));
+  }
+
+  async function post(url, value) {
+    const data = new URLSearchParams(value);
+    fetch(url, { method: 'POST', body: data })
+      .then((respons) => respons.json())
+      .then((result) => { return result });
   }
 
   return (
     <>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <Table>
-        <thead>
+        <tr>
+          <Th>번호</Th>
+          <Th>언어</Th>
+          <Th>레벨</Th>
+          <Th>단원</Th>
+          <Th>구분</Th>
+          <Th>한국어</Th>
+          <Th>중국어</Th>
+          <Th>영어</Th>
+          <Th>러시아어</Th>
+          <Th>갱신일자</Th>
+          <Th></Th>
+        </tr>
+        <tr>
+          <Th>{rowm.no}</Th>
+          <Th><Input name='language' value={rowm.language} onChange={handleInputRowm} width='25px' minLength={2} maxLength={2} pattern='^[a-zA-Z]*$' required /></Th>
+          <Th><Input name='level' value={rowm.level} onChange={handleInputRowm} width='25px' minLength={1} maxLength={1} pattern='^[0-9]*$' required /></Th>
+          <Th><Input name='chapter' value={rowm.chapter} onChange={handleInputRowm} width='25px' minLength={2} maxLength={2} pattern='^[0-9]*$' required /></Th>
+          <Th><Input name='gubun' value={rowm.gubun} onChange={handleInputRowm} width='25px' minLength={1} maxLength={1} pattern='^[0-9]*$' required /></Th>
+          <Th><Input name='kl' value={rowm.kl} onChange={handleInputRowm} width='100px' maxLength={50} /></Th>
+          <Th><Input name='cl' value={rowm.cl} onChange={handleInputRowm} width='100px' maxLength={50} /></Th>
+          <Th><Input name='el' value={rowm.el} onChange={handleInputRowm} width='100px' maxLength={50} /></Th>
+          <Th><Input name='rl' value={rowm.rl} onChange={handleInputRowm} width='100px' maxLength={50} /></Th>
+          <Th>{rowm.data}</Th>
+          <Th><Button onClick={insertRow} color='cyan'>추가</Button></Th>
+        </tr>
+        {rows.map((row, index) =>
           <tr>
-            <Th>번호</Th>
-            <Th>언어</Th>
-            <Th>레벨</Th>
-            <Th>단원</Th>
-            <Th>구분</Th>
-            <Th>한국어</Th>
-            <Th>중국어</Th>
-            <Th>영어</Th>
-            <Th>러시아어</Th>
-            <Th>갱신일자</Th>
-            <Th></Th>
+            <Th>{row.no}</Th>
+            <Th><Input name='language' value={row.language} onChange={(event) => handleInputRows(event, index)} width='25px' minLength={2} maxLength={2} pattern='^[a-zA-Z]*$' required /></Th>
+            <Th><Input name='level' value={row.level} onChange={(event) => handleInputRows(event, index)} width='25px' minLength={1} maxLength={1} pattern='^[0-9]*$' required /></Th>
+            <Th><Input name='chapter' value={row.chapter} onChange={(event) => handleInputRows(event, index)} width='25px' minLength={2} maxLength={2} pattern='^[0-9]*$' required /></Th>
+            <Th><Input name='gubun' value={row.gubun} onChange={(event) => handleInputRows(event, index)} width='25px' minLength={1} maxLength={1} pattern='^[0-9]*$' required /></Th>
+            <Th><Input name='kl' value={row.kl} onChange={(event) => handleInputRows(event, index)} width='100px' maxLength={50} /></Th>
+            <Th><Input name='cl' value={row.cl} onChange={(event) => handleInputRows(event, index)} width='100px' maxLength={50} /></Th>
+            <Th><Input name='el' value={row.el} onChange={(event) => handleInputRows(event, index)} width='100px' maxLength={50} /></Th>
+            <Th><Input name='rl' value={row.rl} onChange={(event) => handleInputRows(event, index)} width='100px' maxLength={50} /></Th>
+            <Th>{row.data}</Th>
+            <Th><Button onClick={row.btnOpt ? () => updateRow(index) : () => deleteRow(index)} color={row.btnOpt ? 'lightgreen' : 'red'}>{row.btnOpt ? '수정' : '삭제'}</Button></Th>
           </tr>
-          <tr>
-            <Th><span>{rowMain.no}</span></Th>
-            <Th><Input className='w25px' value={rowMain.language} onInput={(event) => setRowMain({ ...rowMain, language: toEng(event.target.value) })} maxLength={2} /></Th>
-            <Th><Input className='w25px' value={rowMain.level} onInput={(event) => setRowMain({ ...rowMain, level: toNum(event.target.value) })} maxLength={1} /></Th>
-            <Th><Input className='w25px' value={rowMain.chapter} onInput={(event) => setRowMain({ ...rowMain, chapter: toNum(event.target.value) })} maxLength={2} /></Th>
-            <Th><Input className='w25px' value={rowMain.gubun} onInput={(event) => setRowMain({ ...rowMain, gubun: toNum(event.target.value) })} maxLength={1} /></Th>
-            <Th><Input className='w150px' value={rowMain.kl} onInput={(event) => setRowMain({ ...rowMain, kl: event.target.value })} maxLength={50} /></Th>
-            <Th><Input className='w150px' value={rowMain.cl} onInput={(event) => setRowMain({ ...rowMain, cl: event.target.value })} maxLength={50} /></Th>
-            <Th><Input className='w150px' value={rowMain.el} onInput={(event) => setRowMain({ ...rowMain, el: event.target.value })} maxLength={50} /></Th>
-            <Th><Input className='w150px' value={rowMain.rl} onInput={(event) => setRowMain({ ...rowMain, rl: event.target.value })} maxLength={50} /></Th>
-            <Th><span>{rowMain.date}</span></Th>
-            <Th><Button className='insert' onClick={insertRow}>추가</Button></Th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((rowMap, index) =>
-            <tr>
-              <Th><span>{rowMap.no}</span></Th>
-              <Th><Input className='w25px' value={rowMap.language} onInput={(event) => setRows(newArr(index, { ...rowMap, language: toEng(event.target.value), btnOpt: true }))} maxLength={2} /></Th>
-              <Th><Input className='w25px' value={rowMap.level} onInput={(event) => setRows(newArr(index, { ...rowMap, level: toNum(event.target.value), btnOpt: true }))} maxLength={1} /></Th>
-              <Th><Input className='w25px' value={rowMap.chapter} onInput={(event) => setRows(newArr(index, { ...rowMap, chapter: toNum(event.target.value), btnOpt: true }))} maxLength={2} /></Th>
-              <Th><Input className='w25px' value={rowMap.gubun} onInput={(event) => setRows(newArr(index, { ...rowMap, gubun: toNum(event.target.value), btnOpt: true }))} maxLength={1} /></Th>
-              <Th><Input className='w150px' value={rowMap.kl} onInput={(event) => setRows(newArr(index, { ...rowMap, kl: event.target.value, btnOpt: true }))} maxLength={50} /></Th>
-              <Th><Input className='w150px' value={rowMap.cl} onInput={(event) => setRows(newArr(index, { ...rowMap, cl: event.target.value, btnOpt: true }))} maxLength={50} /></Th>
-              <Th><Input className='w150px' value={rowMap.el} onInput={(event) => setRows(newArr(index, { ...rowMap, el: event.target.value, btnOpt: true }))} maxLength={50} /></Th>
-              <Th><Input className='w150px' value={rowMap.rl} onInput={(event) => setRows(newArr(index, { ...rowMap, rl: event.target.value, btnOpt: true }))} maxLength={50} /></Th>
-              <Th><span>{rowMap.date}</span></Th>
-              <Th><Button className={rowMap.btnOpt ? 'updete' : 'delete'} onClick={rowMap.btnOpt ? () => updateRow(index) : () => deleteRow(index)}>{rowMap.btnOpt ? '수정' : '삭제'}</Button></Th>
-            </tr>
-          )}
-        </tbody>
+        )}
       </Table>
     </>
   );
 }
+
+const Input = styled.input`
+    width: ${(props) => props.width};
+    outline: none;
+    border: none;
+    border-bottom: 1px solid gray;
+    background-color: rgba(0, 0, 0, 0);
+    font-family: 'NanumGothicBold';
+    color: white;
+    font-size: 15px;
+
+    &:focus {
+        border-color: white;
+    }
+
+    &:invalid {
+        border-color: red;
+    }
+`;
+
+const Button = styled.button`
+    cursor: pointer;
+    border: none;
+    background-color: rgba(0, 0, 0, 0);
+    font-family: 'NanumGothicBold';
+    color: ${(props) => props.color};
+    font-size: 15px;
+
+    &:active {
+        position: relative;
+        top: 1px;
+    }
+`;
 
 const Table = styled.table`
   margin-left: auto;
@@ -186,48 +195,6 @@ const Table = styled.table`
 const Th = styled.th`
   border-bottom: 1px solid white;
   padding: 10px;
-`;
-
-const Input = styled.input`
-  outline: none;
-  border: none;
-  border-bottom: 1px solid gray;
-  background-color: rgba(0, 0, 0, 0);
-
-  &.w25px {
-    width: 25px;
-  }
-
-  &.w150px {
-    width: 150px;
-  }
-
-  &:focus {
-    border-color: white;
-  }
-`;
-
-const Button = styled.button`
-  cursor: pointer;
-  border: none;
-  background-color: rgba(0, 0, 0, 0);
-
-  &.insert {
-    color: cyan;
-  }
-
-  &.update {
-    color: lightgreen;
-  }
-
-  &.delete {
-    color: lightcoral;
-  }
-
-  &:active {
-    position: relative;
-    top: 1px;
-  }
 `;
 
 export default WordTable;
