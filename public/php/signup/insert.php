dfc61e57edc5ab09289cc7c03ce2d5d3aa32b484
Taeyeong Sun <styd4957@gmail.com>
2023-04-22 22:12:21 +0900

    20230422

--- public/php/signup/insert.php
+++ public/php/signup/insert.php
@@ -12,26 +12,26 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             $_POST['date']
         ];
 
-        $sql = 'SELECT COUNT(id) as :bind FROM member GROUP BY id HAVING COUNT(id) > 1';
+        $sql = 'SELECT COUNT(id) as :id FROM member GROUP BY id HAVING COUNT(id) > 1';
         $stmt = $connect->prepare($sql);
-        $stmt->bindValue(':bind', $valueArr[0], PDO::PARAM_STR);
+        $stmt->bindValue(':id', $valueArr[0], PDO::PARAM_STR);
         $stmt->execute();
         $count = $stmt->rowCount();
-
         if ($count > 0) {
             header('Content-Type: application/json');
             echo json_encode('DUPLICATION');
-        } else {
-            $sql = 'INSERT INTO member (id, password, email, irum, nickname, date) VALUES (:v0, :v1, :v2, :v3, :v4, :v5)';
-            $stmt = $connect->prepare($sql);
-            foreach ($valueArr as $index => $value) {
-                $stmt->bindValue(':v' . $index, $value, PDO::PARAM_STR);
-            }
-            $stmt->execute();
-
-            header('Content-Type: application/json');
-            echo json_encode('SUCCESS');
+            exit;
         }
+
+        $sql = 'INSERT INTO member (id, password, email, irum, nickname, date) VALUES (:v0, :v1, :v2, :v3, :v4, :v5)';
+        $stmt = $connect->prepare($sql);
+        foreach ($valueArr as $index => $value) {
+            $stmt->bindValue(':v' . $index, $value, PDO::PARAM_STR);
+        }
+        $stmt->execute();
+
+        header('Content-Type: application/json');
+        echo json_encode('SUCCESS');
     } catch (PDOException $e) {
         echo '';
         exit;
